304a508ab6af76adaea9baf4f4d6e866449c6b55
cowai5 <iwaoyuki1205@gmail.com>
2021-12-06 03:01:25 +0900

    :sparkles: 色付け係GAS作成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,73 @@
-const greeter = (person: string) => {
-  return `Hello, ${person}!`;
-};
+function updateEventColor() {
+  const timeMinDate = new Date();
+  timeMinDate.setHours(timeMinDate.getHours() - 2);
+  const optionalArgs = {
+    // timeMin: dayjs().subtract(15, 'minute').format(),
+    timeMin: timeMinDate.toISOString(),
+    showDeleted: false,
+    singleEvents: true,
+    maxResults: 5,
+    orderBy: 'startTime',
+  };
+  const calendarId = 'primary';
 
-function testGreeter() {
-  const user = 'Grant';
-  Logger.log(greeter(user));
+  const events = Calendar.Events?.list(calendarId, optionalArgs);
+  events?.items?.forEach((event: GoogleAppsScript.Calendar.Schema.Event) => {
+    if (!event.colorId) {
+      event.colorId = colorBySummary(event.summary);
+      Calendar.Events?.update(event, calendarId, event.id!);
+    }
+  });
+}
+
+function colorBySummary(summary?: string): string | undefined {
+  // https://developers.google.com/apps-script/reference/calendar/event-color
+
+  if (!summary || /無駄/.test(summary)) {
+    return CalendarApp.EventColor.RED.toString();
+  }
+
+  if (/睡眠/.test(summary)) {
+    return CalendarApp.EventColor.PALE_BLUE.toString();
+  }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.PALE_GREEN.toString();
+  // }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.MAUVE.toString();
+  // }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.PALE_RED.toString();
+  // }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.YELLOW.toString();
+  // }
+
+  if (/英会話|英語|Anki|ELSA/.test(summary)) {
+    return CalendarApp.EventColor.ORANGE.toString();
+  }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.CYAN.toString();
+  // }
+
+  if (
+    /(おむつ|ミルク|あやし|寝かしつけ|ごみ|ゴミ|綿棒|沐浴|炊事|洗濯|洗い物)/.test(
+      summary
+    )
+  ) {
+    return CalendarApp.EventColor.GRAY.toString();
+  }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.BLUE.toString();
+  // }
+
+  // if (/word/.test(summary)) {
+  //   return CalendarApp.EventColor.GREEN.toString();
+  // }
 }
